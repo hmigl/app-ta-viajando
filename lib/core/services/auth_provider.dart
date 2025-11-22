ce78953e5ae7955c2c7652fa65ca1777708f61e0
Jorge <jgomes.silva1107@gmail.com>
2025-11-22 20:56:22 -0300

    fix path

--- lib/core/services/auth_provider.dart
+++ lib/core/services/auth_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-part '../../auth_provider.g.dart';
+part 'auth_provider.g.dart';
 
 @riverpod
 Stream<AuthState> authState(Ref ref) {
